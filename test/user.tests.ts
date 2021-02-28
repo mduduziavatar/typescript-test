ae8b2a3fb089adc69aefac0a06af117aa170fff2
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2021-02-28 16:05:55 +0200

    added more tests

--- test/user.tests.ts
+++ test/user.tests.ts
@@ -1,6 +1,11 @@
 import assert from 'assert';
 import greet from '../greet';
-import GreetInXhosa from '../greetClass';
+// import GreetInXhosa from '../greetClass';
+// import GreetInEnglish from '../greetClass';
+// export * from "../greetClass"; // exports 'StringValidator' interface
+import { GreetInXhosa, GreetInEnglish } from "../greetClass";
+
+
 // import GreetInEnglish from '../greetClass';
 // import GreetInZulu from '../greetClass';
 
@@ -16,6 +21,10 @@ describe('My first typescript basic test', function () {
     it('should greet in Xhosa', function () {
         const greetInXhosa = new GreetInXhosa();
         assert.equal("Molo, Mike", greetInXhosa.greet("Mike"));
+    });
+    it('should greet in English', function () {
+        const greetInEnglish = new GreetInEnglish();
+        assert.equal("Hello, Mike", greetInEnglish.greet("Mike"));
     });
     
 });
