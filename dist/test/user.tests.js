7a3fad582b4b491807785650a0427fff62b9d6c7
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2021-02-28 16:07:06 +0200

    added more tests

--- dist/test/user.tests.js
+++ dist/test/user.tests.js
@@ -5,7 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const assert_1 = __importDefault(require("assert"));
 const greet_1 = __importDefault(require("../greet"));
-const greetClass_1 = __importDefault(require("../greetClass"));
+// import GreetInXhosa from '../greetClass';
+// import GreetInEnglish from '../greetClass';
+// export * from "../greetClass"; // exports 'StringValidator' interface
+const greetClass_1 = require("../greetClass");
+// import GreetInEnglish from '../greetClass';
+// import GreetInZulu from '../greetClass';
 describe('My first typescript basic test', function () {
     it('should test strings', function () {
         assert_1.default.equal("Hello, Bob Crow we can't contact you.", greet_1.default({
@@ -14,7 +19,11 @@ describe('My first typescript basic test', function () {
         }));
     });
     it('should greet in Xhosa', function () {
-        const greetInXhosa = new greetClass_1.default();
+        const greetInXhosa = new greetClass_1.GreetInXhosa();
         assert_1.default.equal("Molo, Mike", greetInXhosa.greet("Mike"));
+    });
+    it('should greet in English', function () {
+        const greetInEnglish = new greetClass_1.GreetInEnglish();
+        assert_1.default.equal("Hello, Mike", greetInEnglish.greet("Mike"));
     });
 });
