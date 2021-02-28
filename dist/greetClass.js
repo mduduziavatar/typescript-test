8917ed6d2997118eb921f5633abe8ac56111106e
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2021-02-28 16:06:31 +0200

    added tests

--- dist/greetClass.js
+++ dist/greetClass.js
@@ -1,18 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.GreetInEnglish = exports.GreetInZulu = exports.GreetInXhosa = void 0;
 class GreetInXhosa {
     greet(name) {
         return "Molo, " + name;
     }
 }
-exports.default = GreetInXhosa;
+exports.GreetInXhosa = GreetInXhosa;
 class GreetInZulu {
     greet(name) {
         return "Sawubona, " + name;
     }
 }
+exports.GreetInZulu = GreetInZulu;
 class GreetInEnglish {
     greet(name) {
         return "Hello, " + name;
     }
 }
+exports.GreetInEnglish = GreetInEnglish;
+// export * from "./greet";
